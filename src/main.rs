2763ac6bfc021e3340807eb2adffdadeab991203
Giles Cope <gilescope@gmail.com>
2020-04-27 07:52:28 +0100

    Added --verbose / -v flag

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,22 @@ use std::path::Path;
 use subprocess::Exec;
 type DynErr = Box<dyn std::error::Error + 'static>;
 
+struct Config {
+    verbose: bool,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config { verbose: false }
+    }
+}
+
 fn main() -> Result<(), DynErr> {
     let mut dir: &Path = &env::current_dir()?;
     while !Path::join(dir, Path::new(".gitlab-ci.yml")).exists() {
         dir = dir.parent().unwrap();
     }
+
     let args: Vec<String> = std::env::args().collect();
 
     let target = if args.len() < 2 {
@@ -18,16 +29,30 @@ fn main() -> Result<(), DynErr> {
     } else {
         Some(args[1].to_owned())
     };
-    run(&Path::join(&dir, Path::new(".gitlab-ci.yml")), target)
+    let mut config = Config::default();
+    for arg in &args {
+        if arg == "--verbose" || arg == "-v" {
+            config.verbose = true;
+        }
+    }
+    run(
+        &Path::join(&dir, Path::new(".gitlab-ci.yml")),
+        target,
+        &config,
+    )
 }
 
-fn run(gitlab_file: &Path, job: Option<String>) -> Result<(), Box<dyn std::error::Error>> {
+fn run(
+    gitlab_file: &Path,
+    job: Option<String>,
+    config: &Config,
+) -> Result<(), Box<dyn std::error::Error>> {
     let gitlab_config = gitlab_ci_parser::parse(gitlab_file)?;
 
     if let Some(job) = job {
         for (key, j) in gitlab_config.jobs.iter() {
             if key == &job || j.stage.is_some() && (&job == j.stage.as_ref().unwrap()) {
-                run_job(&gitlab_config, j);
+                run_job(&gitlab_config, j, config);
             }
         }
     } else {
@@ -64,8 +89,8 @@ fn set_vars(job: &Job, mut vars: &mut HashMap<String, String>) {
     }
 }
 
-fn run_job(config: &GitlabCIConfig, j: &Job) {
-    let mut local_vars = config.variables.clone();
+fn run_job(gitlab_config: &GitlabCIConfig, j: &Job, config: &Config) {
+    let mut local_vars = gitlab_config.variables.clone();
 
     set_vars(&j, &mut local_vars);
 
@@ -74,20 +99,26 @@ fn run_job(config: &GitlabCIConfig, j: &Job) {
     }
 
     if let Some(ref script) = j.before_script {
-        run_script(script, &local_vars);
+        run_script(script, &local_vars, config);
     }
 
     if let Some(ref script) = j.script {
-        run_script(script, &local_vars);
+        run_script(script, &local_vars, config);
     }
 }
 
-fn run_script(script: &Vec<String>, local_vars: &HashMap<String, String>) {
+fn run_script(script: &Vec<String>, local_vars: &HashMap<String, String>, config: &Config) {
     for line in script {
         let mut proc = Exec::shell(OsString::from(line));
         for (key, value) in local_vars.iter() {
             proc = proc.env(key, value);
+            if config.verbose {
+                println!("Env: {}={}", key, value)
+            };
         }
+        if config.verbose {
+            println!("Cmd: {}", line)
+        };
         proc.join().unwrap();
     }
 }
@@ -101,20 +132,20 @@ pub mod tests {
     pub fn hello() -> Result<(), DynErr> {
         let root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
         let p = &PathBuf::from(Path::join(&root, "examples/simple/.gitlab-ci.yml"));
-        run(p, "print_hello")
+        run(p, Some("print_hello".into()), &Config::default())
     }
 
     #[test]
     pub fn goodbye_with_variable() -> Result<(), DynErr> {
         let root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
         let p = &PathBuf::from(Path::join(&root, "examples/simple/.gitlab-ci.yml"));
-        run(p, "print_goodbye")
+        run(p, Some("print_goodbye".into()), &Config::default())
     }
 
     #[test]
     pub fn all_in_stage_run() -> Result<(), DynErr> {
         let root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
         let p = &PathBuf::from(Path::join(&root, "examples/simple/.gitlab-ci.yml"));
-        run(p, "primary_stage")
+        run(p, Some("primary_stage".into()), &Config::default())
     }
 }
