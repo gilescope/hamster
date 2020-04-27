75f564142da017fd8901fef645bf708228b9b735
Giles Cope <gilescope@gmail.com>
2020-04-27 13:51:56 +0100

    Much simpler code - tracing seems worth a couple of dependencies.

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,10 @@ use std::env;
 use std::ffi::OsString;
 use std::path::Path;
 use subprocess::Exec;
+use tracing::{debug, info, Level};
+use tracing_subscriber;
+
 type DynErr = Box<dyn std::error::Error + 'static>;
-
-struct Config {
-    verbose: bool,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Config { verbose: false }
-    }
-}
 
 fn main() -> Result<(), DynErr> {
     let mut dir: &Path = &env::current_dir()?;
@@ -29,35 +22,37 @@ fn main() -> Result<(), DynErr> {
     let target = if args.len() < 2 {
         None
     } else {
+        if args[1] == "--version" || args[1] == "-v" {
+            println!("hamster v{}", env!("CARGO_PKG_VERSION"));
+        }
         Some(args[1].to_owned())
     };
-    let mut config = Config::default();
-    if args[1] == "--version" || args[1] == "-v" {
-        println!("hamster v{}", env!("CARGO_PKG_VERSION"));
-    }
+
+    let mut lev = Level::INFO;
     for arg in &args {
         if arg == "--verbose" || arg == "-v" {
-            config.verbose = true;
+            lev = Level::TRACE;
         }
     }
-    run(
-        &Path::join(&dir, Path::new(".gitlab-ci.yml")),
-        target,
-        &config,
-    )
+    let subscriber = tracing_subscriber::fmt()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(lev)
+        // builds the subscriber.
+        .finish();
+
+    tracing::subscriber::with_default(subscriber, || {
+        run(&Path::join(&dir, Path::new(".gitlab-ci.yml")), target)
+    })
 }
 
-fn run(
-    gitlab_file: &Path,
-    job: Option<String>,
-    config: &Config,
-) -> Result<(), Box<dyn std::error::Error>> {
+fn run(gitlab_file: &Path, job: Option<String>) -> Result<(), Box<dyn std::error::Error>> {
     let gitlab_config = gitlab_ci_parser::parse(gitlab_file)?;
 
     if let Some(job) = job {
         for (key, j) in gitlab_config.jobs.iter() {
             if key == &job || j.stage.is_some() && (&job == j.stage.as_ref().unwrap()) {
-                run_job(&gitlab_config, j, config);
+                run_job(&gitlab_config, j);
             }
         }
     } else {
@@ -94,7 +89,7 @@ fn set_vars(job: &Job, mut vars: &mut HashMap<String, String>) {
     }
 }
 
-fn run_job(gitlab_config: &GitlabCIConfig, j: &Job, config: &Config) {
+fn run_job(gitlab_config: &GitlabCIConfig, j: &Job) {
     let mut local_vars = gitlab_config.variables.clone();
 
     set_vars(&j, &mut local_vars);
@@ -104,26 +99,22 @@ fn run_job(gitlab_config: &GitlabCIConfig, j: &Job, config: &Config) {
     }
 
     if let Some(ref script) = j.before_script {
-        run_script(script, &local_vars, config);
+        run_script(script, &local_vars);
     }
 
     if let Some(ref script) = j.script {
-        run_script(script, &local_vars, config);
+        run_script(script, &local_vars);
     }
 }
 
-fn run_script(script: &Vec<String>, local_vars: &HashMap<String, String>, config: &Config) {
+fn run_script(script: &Vec<String>, local_vars: &HashMap<String, String>) {
     for line in script {
         let mut proc = Exec::shell(OsString::from(line));
         for (key, value) in local_vars.iter() {
             proc = proc.env(key, value);
-            if config.verbose {
-                println!("Env: {}={}", key, value)
-            };
+            debug!("Env: {}={}", key, value);
         }
-        if config.verbose {
-            println!("Cmd: {}", line)
-        };
+        info!("Cmd: {}", line);
         proc.join().expect("Process returned non-zero exit code");
     }
 }
@@ -132,25 +123,32 @@ fn run_script(script: &Vec<String>, local_vars: &HashMap<String, String>, config
 pub mod tests {
     use super::*;
     use std::path::PathBuf;
+    //use subprocess::Exec;
 
     #[test]
     pub fn hello() -> Result<(), DynErr> {
         let root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
         let p = &PathBuf::from(Path::join(&root, "examples/simple/.gitlab-ci.yml"));
-        run(p, Some("print_hello".into()), &Config::default())
+        run(p, Some("print_hello".into()))
     }
 
     #[test]
     pub fn goodbye_with_variable() -> Result<(), DynErr> {
         let root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
         let p = &PathBuf::from(Path::join(&root, "examples/simple/.gitlab-ci.yml"));
-        run(p, Some("print_goodbye".into()), &Config::default())
+        run(p, Some("print_goodbye".into()))
     }
 
     #[test]
     pub fn all_in_stage_run() -> Result<(), DynErr> {
         let root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
         let p = &PathBuf::from(Path::join(&root, "examples/simple/.gitlab-ci.yml"));
-        run(p, Some("primary_stage".into()), &Config::default())
+        run(p, Some("primary_stage".into()))
     }
+
+    // #[test]
+    // pub fn no_args() {
+    //     let exe: &'static str = env!("CARGO_BIN_EXE_HAMSTER");
+    //     assert!(Exec::cmd(exe).join().is_ok());
+    // }
 }
